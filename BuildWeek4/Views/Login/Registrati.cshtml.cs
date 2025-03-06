5bc4af805451b01845d61da9b292fe33bdbfd22a
Giuseppe <Giuseppe@WIN-SOLSLKA85KI>
2025-03-06 10:49:03 +0100

    primo commit

---
+++ BuildWeek4/Views/Login/Registrati.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BuildWeek4.Views.Login
+{
+    public class RegistratiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
